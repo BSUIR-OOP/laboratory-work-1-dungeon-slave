c008310edb8d4c260ab99b5bc6682cbf7ffcaa44
dungeon-slave <70040363+dungeon-slave@users.noreply.github.com>
2022-03-15 15:18:56 +0300

    Add files via upload

---
+++ FigureList.cs
@@ -0,0 +1,19 @@
+﻿class FiguresList
+{
+    private Dot[] FiguresArray = new Dot[6];
+    private int pos = 0;
+
+    public void AddFigureToList(Dot Figure)
+    {
+        FiguresArray[pos] = Figure;
+        pos++;
+    }
+    public void PrintFigures(Render Rn)
+    {
+        while(pos > 0)
+        {
+            pos--;
+            System.Console.WriteLine(Rn.FrameRendering(FiguresArray[pos].IsFigure));
+        }
+    }
+}
