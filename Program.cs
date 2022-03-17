f0a8321a84dad5718e84110fd5e7cba6c3f78be6
dungeon-slave <70040363+dungeon-slave@users.noreply.github.com>
2022-03-17 17:47:00 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,20 +1,18 @@
-﻿using System;
-
 class Program
 {
     static void Main()
     {
         FiguresList List = new();
 
-        List.AddFigureToList(new Dot(0, 0));
-        List.AddFigureToList(new Line(0f, 0f));
-        List.AddFigureToList(new Circle(0f, 0f));
-        List.AddFigureToList(new Rectangle(0f, 0f));
-        List.AddFigureToList(new Treangle(0f, 0f));
-        List.AddFigureToList(new Rhombus(0f, 0f));
+        List.AddToList(new Dot(0, 0));
+        List.AddToList(new Line(0f, 0f));
+        List.AddToList(new Circle(0f, 0f));
+        List.AddToList(new Rectangle(0f, 0f));
+        List.AddToList(new Treangle(0f, 0f));
+        List.AddToList(new Rhombus(0f, 0f));
 
-        List.PrintFigures(new Render());
+        List.PrintList(new Render());
 
-        Console.ReadLine();
+        System.Console.ReadLine();
     }
 }
